af33992c0a54c5ce854b57152500bf8b28514ca5
Valmik <KStorer61@icloud.com>
2022-07-13 21:41:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', calculateTime)
+
+function calculateTime(){
+    var date = new Date();
+    var dayNumber = date.getDay();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var sec = date.getSeconds();
+    var ampm = hour >= 12 ? 'ВЕЧЕР' : 'УТРО';
+    var dayNames = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+
+    hour = hour % 12;
+    hour = hour ? 12 : '12';
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+
+    document.getElementById("day").innerHTML = dayNames[dayNumber];
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("sec").innerHTML = sec;
+    document.getElementById("ampm").innerHTML = ampm;
+
+    setTimeout(calculateTime, 200);
+}
